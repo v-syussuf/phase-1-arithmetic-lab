856eca3656482d37b7de049633f76df00574ad93
Yussuf <yussuf.saumu@gmail.com>
2023-09-21 18:29:41 +0300

    Completed Message

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+function multiply(num1, num2){
+    return num1 * num2; 
+}  
+   const result = multiply(31,2);
 
-// Write your code here
+function random(){
+    let random = Math.random() != 0;
+
+    console.log(random);
+}
+function mod(num3, num4){
+    return num3 % num4;
+  }
+    const num3 = 31;
+    const num4 = 4;
+  
+    const results = mod(num3,num4);
+
+    function max(){
+        const num = [13,6,7,20,18,12,19,4,1];
+        const max = Math.max(13,6,7,20,18,12,19,4,1);
+    
+        console.log(`The highest number in the set is ${max}`)
+    }
+    max();
---
+++ test/Untitled-1.js
@@ -0,0 +1,17 @@
+
+function multiply(num1, num2){
+    return num1 * num2;
+   
+}  
+   const num1 =31;
+   const num2 =2;
+   
+   const result = multiply(num1,num2);
+
+function max(){
+    const num = [13,6,7,20,18,12,19,4,1];
+    const max = Math.max(13,6,7,20,18,12,19,4,1);
+
+    console.log(`The highest number in the set is ${max}`);
+}
+  
